3871c68964845baacb5a1d74ef40f2b60d79bd1b
cuongus18 <cuongungsy@gmail.com>
2019-03-02 11:00:27 +0700

    Update 01/03/2019

---
+++ json/tin_tuc.js
@@ -0,0 +1,87 @@
+var tintuc_data = [
+{
+        icon_pic : "https://farm8.staticflickr.com/7853/32273667207_62b8d170b5_m.jpg",
+        title : "Long Chiến Thương Khung - 군도모바일: gMO PK liên server 24/7 chính thức mở tải, sẵn sàng ra mắt 20/02",
+        date : "20/02/2019",
+        time : "18:20"
+},
+{
+        icon_pic : "https://farm8.staticflickr.com/7853/32273667207_62b8d170b5_m.jpg",
+        title : "Bom tấn chiến thuật SLG Top 1 Châu Á Long Đồ Bá Nghiệp chính thức ra mắt HÔM NAY, tặng Gift Code  ",
+        date : "19/02/2019",
+        time : "18:20"
+},
+{
+        icon_pic : "https://farm8.staticflickr.com/7853/32273667207_62b8d170b5_m.jpg",
+        title : "[Long Đồ Bá Nghiệp] Ra Mắt Trailer hoành tráng, Ấn định ngày ra mắt 22/1/2019",
+        date : "11/02/2019",
+        time : "18:20"
+},
+{
+        icon_pic : "https://farm8.staticflickr.com/7853/32273667207_62b8d170b5_m.jpg",
+        title : "Đến từ NSX Top đầu Châu Á, Long Đồ Bá Nghiệp sẽ là game chiến thuật SLG đầu tiên đẹp tựa như phim ",
+        date : "10/02/2019",
+        time : "18:20"
+},
+{
+        icon_pic : "https://farm8.staticflickr.com/7853/32273667207_62b8d170b5_m.jpg",
+        title : "Bom tấn chiến thuật SLG Top đầu Châu Á: Long Đồ Bá Nghiệp chính thức được mua về Việt Nam ",
+        date : "09/02/2019",
+        time : "18:20"
+},
+{
+        icon_pic : "https://farm8.staticflickr.com/7853/32273667207_62b8d170b5_m.jpg",
+        title : "Long Chiến Thương Khung - 군도모바일: gMO PK liên server 24/7 chính thức mở tải, sẵn sàng ra mắt 20/02",
+        date : "08/02/2019",
+        time : "18:20"
+},
+{
+        icon_pic : "https://farm8.staticflickr.com/7853/32273667207_62b8d170b5_m.jpg",
+        title : "Bom tấn chiến thuật SLG Top đầu Châu Á: Long Đồ Bá Nghiệp chính thức được mua về Việt Nam ",
+        date : "07/02/2019",
+        time : "18:20"
+},
+{
+        icon_pic : "https://farm8.staticflickr.com/7853/32273667207_62b8d170b5_m.jpg",
+        title : "Long Chiến Thương Khung - 군도모바일: gMO PK liên server 24/7 chính thức mở tải, sẵn sàng ra mắt 20/02",
+        date : "06/02/2019",
+        time : "18:20"
+},
+{
+        icon_pic : "https://farm8.staticflickr.com/7853/32273667207_62b8d170b5_m.jpg",
+        title : "[Đặc Biệt] Ra mắt MV Người Làng Lá dành tặng riêng cho cộng đồng game thủ",
+        date : "05/02/2019",
+        time : "18:20"
+},
+    {
+        icon_pic : "https://farm8.staticflickr.com/7853/32273667207_62b8d170b5_m.jpg",
+        title : "[LÀNG LÁ PHIÊU LƯU KÝ] SEASON: KẾ HOẠCH NGUYỆT NHÃN ",
+        date : "04/02/2019",
+        time : "18:20"
+    },
+    {
+        icon_pic : "https://farm8.staticflickr.com/7853/32273667207_62b8d170b5_m.jpg",
+        title : "[Thiên Hạ Anh Hùng] Chính Thức Ra Mắt Closed Beta Không Reset 10h ngày (08/08",
+        date : "03/02/2019",
+        time : "18:20"
+    },
+    {
+        icon_pic : "https://farm8.staticflickr.com/7853/32273667207_62b8d170b5_m.jpg",
+        title : "[Cộng Đồng] Những Hình Ảnh Làm Nên Tên Tuổi Làng Lá Trong Lòng Game Thủ Việt ",
+        date : "02/02/2019",
+        time : "18:20"
+    },
+    {
+        icon_pic : "https://farm8.staticflickr.com/7853/32273667207_62b8d170b5_m.jpg",
+        title : "[Thông Báo] Chính thức Đổi Tên Game thành Võ Lâm Anh Hùng Truyện ",
+        date : "01/02/2019",
+        time : "18:20"
+    },
+    {
+        icon_pic : "https://farm8.staticflickr.com/7853/32273667207_62b8d170b5_m.jpg",
+        title : "[Đặc Biệt] Ra mắt MV Người Làng Lá dành tặng riêng cho cộng đồng game thủ",
+        date : "30/01/2019",
+        time : "18:20"
+    }
+
+];
